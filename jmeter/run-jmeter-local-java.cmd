7a0ff8156ff8d4aa86e550847f4c5cba5fb176cf
dannybcoding <danielanderson4815@gmail.com>
2026-07-31 16:52:48 -0500

    updated for new laptop jmeter/load test path

--- jmeter/run-jmeter-local-java.cmd
+++ jmeter/run-jmeter-local-java.cmd
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 echo Starting JMeter load test...
-set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_141"
+set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-25.0.4.7-hotspot"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 if not exist "%~dp0results" mkdir "%~dp0results"
-call "C:\Users\and05\tools\apache-jmeter-5.6.3\bin\jmeter.bat" -n -t "%~dp0teams-page-load-test.jmx" -l "%~dp0results\teams-page-load-test.jtl" -j "%~dp0results\jmeter.log"
+call "C:\Users\danny\Tools\apache-jmeter-5.6.3\bin\jmeter" -n -t "%~dp0teams-page-load-test.jmx" -l "%~dp0results\teams-page-load-test.jtl" -j "%~dp0results\jmeter.log"
 set "EXIT_CODE=%ERRORLEVEL%"
 if "%EXIT_CODE%"=="0" (
   echo JMeter load test complete. See jmeter\results\teams-page-load-test.jtl
